c84499680304bc00f19e51b6e2ccb335313a753f
林志杰 <zigitlam@yeah.net>
2020-06-08 18:25:26 +0800

    Add a new evaluate function to estimate the global testing set.

--- net_2.py
+++ net_2.py
@@ -62,7 +62,6 @@ def getMaskAndLable(train_idx, truth, segments):
     -------
 
     """
-
     n_segments = np.max(segments) + 1
     train_mask = np.full((n_segments,), False, dtype=bool)
     test_mask = np.full((n_segments,), True, dtype=bool)
@@ -72,18 +71,25 @@ def getMaskAndLable(train_idx, truth, segments):
         seg_idx = segments[r, c]
         train_mask[seg_idx] = True
         test_mask[seg_idx] = False
-        seg_y[seg_idx] = label + 1
+        seg_y[seg_idx] = label
 
     for i, _y in enumerate(seg_y):
         if _y == -1:
             mask = (segments == i)
             label = truth[mask].astype(np.int)
             counts = np.bincount(label)
-            seg_y[i] = np.argmax(counts)
+            seg_y[i] = 0 if len(counts)==1 else np.argmax(counts[1:])
             if seg_y[i] == 0:
                 test_mask[i] = False
 
+    seg_y[i] -= 1
     return train_mask, test_mask, seg_y
+
+def getGlobalMask(truth, test_idx):
+    mask = np.zeros_like(truth, dtype=bool)
+    for r, c, _ in test_idx:
+        mask[r, c] = True
+    return mask.flatten()
 
 def showSuperpixel(image, segments):
     numSegments = np.max(segments) + 1
@@ -100,15 +106,31 @@ def showSuperpixel(image, segments):
     # show the plots
     plt.show()
 
-def computeLoss(truth, segement, output):
-    shape = segments.shape
-    segments = np.reshape(segments, (-1, shape[-1]))
-    truth = np.reshape(truth, (-1, shape[-1]))
-    new_output = np.zeros((truth.shape[0], output.shape[-1]))
-
+def computeLoss(truth, segments, output, global_test_mask=None):
+    new_output = torch.zeros((truth.shape[0], output.shape[-1]), dtype=torch.float).to(segments.device)
     for idx, c in enumerate(output):
         new_output[segments == idx] = c
+    if not global_test_mask is None:
+        truth = truth[global_test_mask] - 1
+        new_output = new_output[global_test_mask]
 
+    return F.nll_loss(new_output, truth)
+
+def evalute(model, truth, segments, test_mask):
+    model.eval()
+    maxx = torch.max(segments)
+    _, pred = model(data).max(dim=1)
+    new_pred = torch.zeros((truth.shape[0]), dtype=torch.long).to(pred.device)
+    for idx, p in enumerate(pred):
+        new_pred[segments == idx] = p
+    if not test_mask is None:
+        truth = truth[test_mask] - 1
+        new_pred = new_pred[test_mask]
+
+    correct = float(new_pred.eq(truth).sum().item())
+    acc = correct / test_mask.sum().item()
+    total = test_mask.sum().item()
+    print('Accuracy: {:.4f} | test sample number: {:6}'.format(acc, test_mask.sum().item()))
 
 
 class Net(torch.nn.Module):
@@ -122,8 +144,8 @@ class Net(torch.nn.Module):
         self.bn1 = nn.BatchNorm1d(64)
         self.conv2 = SAGEConv(64, 32)
         self.bn2 = nn.BatchNorm1d(32)
-        self.conv3 = SAGEConv(32, cls_num + 1)
-        self.lc = nn.Linear(32, cls_num)
+        self.conv3 = SAGEConv(32, cls_num)
+        # self.lc = nn.Linear(32, cls_num)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
@@ -149,19 +171,24 @@ if __name__ == "__main__":
     image = StandardScaler().fit_transform(image)
     image = np.reshape(image, i_sp)
 
-    numSegments = 2000
+    numSegments = 2500
+    num_sample = 3
+    num_epoch = 1000
 
-    train_csv = pd.read_csv("data/splitDataset/train/splitPavia_3.csv")
-    test_csv = pd.read_csv("data/splitDataset/test/splitPavia_3.csv")
+    train_csv = pd.read_csv("data/splitDataset/train/splitPavia_{}.csv".format(num_sample))
+    test_csv = pd.read_csv("data/splitDataset/test/splitPavia_{}.csv".format(num_sample))
     training_set = train_csv.loc[:, ["row_0", "col_0", "label_0"]].to_numpy()
     testing_set = test_csv.loc[:, ["row_0", "col_0", "label_0"]].to_numpy()
 
+    global_train_mask = getGlobalMask(DataReader.PaviauRaw().truth, training_set)
+    global_test_mask = getGlobalMask(DataReader.PaviauRaw().truth, testing_set)
     segments, edge_index = getSuperpixelGraph(image, num_segments=numSegments, compactness=2.5, sigma=2)
     train_mask, test_mask, y = getMaskAndLable(training_set, DataReader.PaviauRaw().truth, segments)
     sp_feature = getSuperpixelFeature(DataReader.PaviauRaw().cube, segments)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = Net(5, 9).to(device)
-    x = torch.Tensor(PCA(5).fit_transform(sp_feature))
+    model = Net(103, 9).to(device)
+    # x = torch.Tensor(PCA(5).fit_transform(sp_feature))
+    x = torch.Tensor(sp_feature)
     edge_index = torch.tensor(edge_index).t().contiguous()
 
     showSuperpixel(image, segments)
@@ -171,21 +198,26 @@ if __name__ == "__main__":
     data.train_mask = torch.tensor(train_mask)
     data.y = torch.tensor(y)
     data = data.to(device)
+    gpu_segments = torch.tensor(segments).flatten().to(device)
+    global_train_mask = torch.tensor(global_train_mask).to(device)
+    gpu_truth = torch.tensor(DataReader.PaviauRaw().truth, dtype=torch.long).flatten().to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-3)
 
     model.train()
-    for epoch in range(5000):
+    for epoch in range(num_epoch):
         optimizer.zero_grad()
         out = model(data)
-        print(out.shape)
-        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        # loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        loss = computeLoss(gpu_truth, gpu_segments, out, global_train_mask)
         loss.backward()
-        print("loss: ", loss.item())
+        if (epoch+1) % 1 == 0:
+            print("[epoch: {:4}] loss: {}".format(epoch+1, loss.item()))
         optimizer.step()
 
-    model.eval()
-    _, pred = model(data).max(dim=1)
-    correct = float(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
-    acc = correct / data.test_mask.sum().item()
-    print('Accuracy: {:.4f}'.format(acc))
+    # model.eval()
+    # _, pred = model(data).max(dim=1)
+    # correct = float(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+    # acc = correct / data.test_mask.sum().item()
+    # print('Accuracy: {:.4f}'.format(acc))
+    evalute(model, gpu_truth, gpu_segments, global_test_mask)
